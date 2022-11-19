78948ed995d503750054fc1a743568d7b18738b2
Adam Wasylewicz <adwas33@wp.pl>
2022-11-19 12:25:50 +0100

    Init commit

---
+++ src/main/java/com/example/bioconnect/BioConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bioconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bioconnect/BioConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bioconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
