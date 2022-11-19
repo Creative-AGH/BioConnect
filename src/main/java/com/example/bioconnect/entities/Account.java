4cad3b89b36cfa004fe95dc59e1f95b7dc4d2177
Patryk <patrykjanas27@gmail.com>
2022-11-20 00:00:24 +0100

    add test data

--- src/main/java/com/example/bioconnect/entities/Account.java
+++ src/main/java/com/example/bioconnect/entities/Account.java
@@ -1,12 +1,14 @@
 package com.example.bioconnect.entities;
 
 import com.example.bioconnect.Role;
-import com.example.bioconnect.entities.BioWasteHistory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,18 +23,21 @@ public class Account {
     @Enumerated
     private Role role;
 
+    public Account(String email, String password, boolean isVerified) {
+        this.email = email;
+        this.password = password;
+        this.isVerified = isVerified;
+    }
+
     public Account(String email, String password, Role role) {
         this.email = email;
         this.password = password;
         this.role = role;
     }
+
     boolean isVerified;
 
     @OneToMany//(mappedBy = "item") //one item can have many of histories
 //    @JoinColumn(name="itemId") //it indicates a mapped column from Many side //foreign key in history table
     List<BioWasteHistory> history = new ArrayList<>(); //FIXME we dont know if we do that with aspect concept of programming
-
-    public List<BioWasteHistory> getHistory() {
-        return history;
-    }
 }
