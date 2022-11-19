3c68f3f99f633c1b44a24390c11d757af0767163
Patryk <patrykjanas27@gmail.com>
2022-11-20 00:01:16 +0100

    refactor

--- src/main/java/com/example/bioconnect/entities/Account.java
+++ src/main/java/com/example/bioconnect/entities/Account.java
@@ -37,7 +37,6 @@ public class Account {
 
     boolean isVerified;
 
-    @OneToMany//(mappedBy = "item") //one item can have many of histories
-//    @JoinColumn(name="itemId") //it indicates a mapped column from Many side //foreign key in history table
+    @OneToMany
     List<BioWasteHistory> history = new ArrayList<>(); //FIXME we dont know if we do that with aspect concept of programming
 }
