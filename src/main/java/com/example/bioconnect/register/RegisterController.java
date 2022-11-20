2d830c57a604218489e6ef5856f030a466778794
Adam Wasylewicz <adwas33@wp.pl>
2022-11-20 02:14:35 +0100

    CORS REMOVED FOR CONTROLLERS

--- src/main/java/com/example/bioconnect/register/RegisterController.java
+++ src/main/java/com/example/bioconnect/register/RegisterController.java
@@ -5,18 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class RegisterController {
     private final RegisterService registerService;
 
     @PostMapping("/all/register")
-    public ResponseEntity<?> register(RegisterNewAccountDto registerNewAccountDto) {
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterNewAccountDto registerNewAccountDto) {
         registerService.registerNewAccount(registerNewAccountDto);
         return ResponseEntity.noContent().build(); //TODO change to ok status with a body
     }
     @GetMapping("/all/validate")
-    public ResponseEntity<Boolean> verifyEmail(String token) {
+    public ResponseEntity<Boolean> verifyEmail(@RequestParam String token) {
         boolean response = registerService.verifyEmail(token);
         if (!response)
             return ResponseEntity.notFound().build();
